817d74aa7755526d02d79b315175da0a111207fc
Rodrigo Ortega Cuesta <rodortcue@gmail.com>
2014-10-03 10:41:32 +0200

    Update

---
+++ app/scripts/controllers.js
@@ -0,0 +1,40 @@
+(function () {
+  'use strict';
+
+  angular.module('blog.controllers', ['blog.services']);
+
+  function PostListCtrl (Post) {
+    this.posts = Post.query();
+  }
+
+  function PostDetailCtrl ($routeParams, Post, Comment) {
+    this.post = {};
+    this.comments = {};
+    this.user = {}
+    // Guardar la referencia
+    var self = this;
+
+    Post.query({ id: $routeParams.postId })
+      .$promise.then(
+        //Success
+        function (data) {
+          self.post = data[0];
+          self.user = User.query({ id: self.user.userId });
+        }
+        //Error
+        function (error) {
+          console.log(error);
+        }
+      );
+
+    this.comments = Comment.query({ postId: $routeParams.postId });
+  }
+  
+  function PostCreateCtrl (Post) {
+    var self = this;
+    this.create = function() {
+      Post.save(self.post);
+    };
+  }
+
+})();
